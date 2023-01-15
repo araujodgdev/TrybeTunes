bd45a09ff12dfb39478edf40f71a2f12325d2f0f
Douglas Araújo <goudxs@gmail.com>
2023-01-15 17:41:19 -0300

    Implementa o Requisito 01 - BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route path="/page/not/found" component={ NotFound } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/search" component={ Search } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/" component={ Login } />
+      </Switch>
+    );
   }
 }
 
