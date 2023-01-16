052fea8530e64d3fd43e538907b304ec2d54d16a
Douglas Araújo <goudxs@gmail.com>
2023-01-15 21:21:29 -0300

    Implementa o requisito 4 - Cria os links de navegação no cabeçalho

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
     const { userName } = this.props;
     return (
-      <header data-testid="header-component">
-        <h1 data-testid="header-user-name">{userName}</h1>
-      </header>
+      <div>
+        <header data-testid="header-component">
+          <h1 data-testid="header-user-name">{userName}</h1>
+        </header>
+        <nav>
+          <Link data-testid="link-to-search" to="/search">
+            {' '}
+            Pesquisa
+            {' '}
+          </Link>
+          <Link data-testid="link-to-favorites" to="/favorites">
+            {' '}
+            Favoritos
+            {' '}
+          </Link>
+          <Link data-testid="link-to-profile" to="/profile">
+            {' '}
+            Perfil
+            {' '}
+          </Link>
+        </nav>
+      </div>
     );
   }
 }
