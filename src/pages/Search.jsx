e84bec8815b11901055b30f058002fa1107aa06e
Douglas Araújo <goudxs@gmail.com>
2023-01-15 21:42:34 -0300

    Implementa o requisito 5 - Cria o formulário para pesquisar artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -7,6 +7,8 @@ export default class Search extends Component {
   state = {
     loading: true,
     userName: '',
+    searchValue: '',
+    validSearch: false,
   };
 
   async componentDidMount() {
@@ -14,11 +16,37 @@ export default class Search extends Component {
     this.setState({ userName: user.name }, () => this.setState({ loading: false }));
   }
 
+  handleSearchInput = ({ target }) => {
+    const { value } = target;
+    const minLength = 2;
+    const hasMinLength = value.length >= minLength;
+    this.setState({ searchValue: value, validSearch: hasMinLength });
+  };
+
   render() {
-    const { loading, userName } = this.state;
+    const { loading, userName, validSearch, searchValue } = this.state;
     return (
       <div data-testid="page-search">
         {loading ? <Loading /> : <Header userName={ userName } />}
+        <form action="">
+          <label htmlFor="search-input">
+            <input
+              name="search-input"
+              type="text"
+              placeholder="Banda ou artista..."
+              data-testid="search-artist-input"
+              onChange={ this.handleSearchInput }
+              value={ searchValue }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ !validSearch }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
